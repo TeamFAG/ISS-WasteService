8fc16d2bd5ca0525ad940436ebee5a2cd1433cb8
Federico Andrucci <federico.andrucci@gmail.com>
2022-08-31 18:35:32 +0200

    request test

---
+++ sprint0/test/it/unibo/tests/TestRequest.java
@@ -0,0 +1,70 @@
+package it.unibo.tests;
+
+import it.unibo.ctxrequest.MainCtxrequestKt;
+import it.unibo.kactor.ActorBasic;
+import it.unibo.kactor.MsgUtil;
+import it.unibo.kactor.QakContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import unibo.comm22.utils.ColorsOut;
+import unibo.comm22.utils.CommSystemConfig;
+import unibo.comm22.utils.CommUtils;
+import ws.Material;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestRequest {
+
+    public final static String HOST = "localhost";
+    public final static int PORT = 8010;
+
+    @Before
+    public void up() {
+        CommSystemConfig.tracing = false;
+
+        new Thread(MainCtxrequestKt::main).start();
+
+        ColorsOut.outappl("wasteservice waiting", ColorsOut.BLUE);
+
+        ActorBasic actorBasic = QakContext.Companion.getActor("wasteservice");
+        while(actorBasic == null) {
+            CommUtils.delay(500);
+            actorBasic = QakContext.Companion.getActor("wasteservice");
+        }
+
+        ColorsOut.outappl("wasteservice ok", ColorsOut.BLUE);
+    }
+
+    @After
+    public void down() {
+        ColorsOut.outappl("Tests done", ColorsOut.BLUE);
+    }
+
+    @Test
+    public void testLoadAccepted() {
+        String reply = simulateRequest(Material.PLASTIC, 10.0);
+        ColorsOut.outappl("Reply: " + reply, ColorsOut.GREEN);
+        assertTrue(reply.contains("loadAccepted"));
+    }
+
+    @Test
+    public void testLoadRejected() {
+        String reply = simulateRequest(Material.GLASS, 150.0);
+        ColorsOut.outappl("Reply: " + reply, ColorsOut.GREEN);
+        assertTrue(reply.contains("loadRejected"));
+    }
+
+    private String simulateRequest(Material material, Double quantity) {
+        String request = MsgUtil.buildRequest("test", "storeRequest", "storeRequest(" + material + ", " + quantity + ")", "wasteservice").toString();
+
+        try {
+            ConnTcp conn = new ConnTcp(HOST, PORT);
+            return conn.request(request);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
