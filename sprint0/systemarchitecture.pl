b56b4de686a71027a11ee6a41a64c5ec1b0bf582
Federico Andrucci <federico.andrucci@gmail.com>
2022-09-05 19:08:43 +0200

    aaaaaaa

---
+++ sprint0/systemarchitecture.pl
@@ -0,0 +1,6 @@
+%====================================================================================
+% systemarchitecture description   
+%====================================================================================
+context(ctxdriver, "localhost",  "TCP", "8010").
+context(ctxwasteservice, "localhost",  "TCP", "8020").
+context(ctxgui, "localhost",  "TCP", "8030").
