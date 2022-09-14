d01231158af22770a78d39090ebd0bc0c47a1fe9
Federico Andrucci <federico.andrucci@gmail.com>
2022-09-14 18:52:26 +0200

    pathexecutor + planner: funzionanti

--- provaPath/provapath.pl
+++
@@ -1,8 +0,0 @@
-%====================================================================================
-% provapath description   
-%====================================================================================
-context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
-context(ctxpath, "localhost",  "TCP", "8030").
- qactor( basicrobot, ctxbasicrobot, "external").
-  qactor( tester, ctxpath, "it.unibo.tester.Tester").
-  qactor( pathexec, ctxpath, "it.unibo.pathexec.Pathexec").
