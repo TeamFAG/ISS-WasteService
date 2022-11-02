8828825c2d9cae0881f866cc9cbab974b64b9112
Federico Andrucci <federico.andrucci@gmail.com>
2022-11-02 20:39:00 +0100

    fixed distr

---
+++ sprint2/sprint2_project/docker/local/sprint2_project.pl
@@ -0,0 +1,22 @@
+%====================================================================================
+% sprint2_project description   
+%====================================================================================
+context(ctxwasteservice, "wasteservice",  "TCP", "8050").
+context(ctxtrolley, "trolley",  "TCP", "8060").
+context(ctxrasp, "rasp",  "TCP", "8070").
+ qactor( sonaremittersimulator, ctxrasp, "wsSonar.sonarEmitterSimulator").
+  qactor( sonaremitterconcrete, ctxrasp, "wsSonar.sonarEmitterConcrete").
+  qactor( wsdatacleaner, ctxrasp, "wsSonar.dataCleaner").
+  qactor( wsdistancefilter, ctxrasp, "wsSonar.distanceFilter").
+  qactor( wasteservice, ctxwasteservice, "it.unibo.wasteservice.Wasteservice").
+  qactor( transporttrolley, ctxtrolley, "it.unibo.transporttrolley.Transporttrolley").
+  qactor( trolleymover, ctxtrolley, "it.unibo.trolleymover.Trolleymover").
+  qactor( pather, ctxtrolley, "it.unibo.pather.Pather").
+  qactor( trolleystateobserver, ctxtrolley, "it.unibo.trolleystateobserver.Trolleystateobserver").
+  qactor( led, ctxrasp, "it.unibo.led.Led").
+  qactor( sonar, ctxrasp, "it.unibo.sonar.Sonar").
+  qactor( halteventshandler, ctxrasp, "it.unibo.halteventshandler.Halteventshandler").
+  qactor( datacleaner, ctxtrolley, "rx.dataCleaner").
+  qactor( distancefilter, ctxtrolley, "rx.distanceFilter").
+  qactor( basicrobot, ctxtrolley, "it.unibo.basicrobot.Basicrobot").
+  qactor( envsonarhandler, ctxtrolley, "it.unibo.envsonarhandler.Envsonarhandler").
