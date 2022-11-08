cf78402695dd77f1702240368b5cacbaa3a98f30
Federico Andrucci <federico.andrucci@gmail.com>
2022-11-08 14:28:24 +0100

    fix sh

--- sprint2/sprint2_project/docker/distributed/prepare-rpi.sh
+++ sprint2/sprint2_project/docker/distributed/prepare-rpi.sh
@@ -2,6 +2,8 @@
 IP="192.168.50.13"
 
 JAR_PATH="../../build/libs/it.unibo.ctxrasp.MainCtxraspKt-1.0.jar"
+DIST_FOLDER="/home/pi/Sviluppo/wsdistr"
+CHECK=""
 
 log() {
     printf "################################################################################\n"
@@ -10,9 +12,21 @@ log() {
 }
 
 copy_resources() {
+  ssh pi@$IP ls /home/pi/Sviluppo | grep wsdistr > CHECK
+
+  if [[ "$CHECK" =~ "wsdistr" ]]; then
+    log("distribution folder already exist... deleting")
+    ssh pi@$IP rm -r /home/pi/Sviluppo/wsdistr
+  fi
+
+  log("Copying distribution files")
+
   ssh pi@$IP mkdir /home/pi/Sviluppo/wsdistr
-  #scp -P 2222 $JAR_PATH pi@$IP:~/Sviluppo/wsdistr
-  #scp -P 2222  pi@$IP:~/Sviluppo/wsdistr
+  scp -P 2222 $JAR_PATH pi@$IP:~/Sviluppo/wsdistr
+  scp -P 2222 ./sprint2_project.pl pi@$IP:~/Sviluppo/wsdistr
+  scp -P 2222 ./SystemConfiguration.json pi@$IP:~/Sviluppo/wsdistr
+
+  log("Copy done")
 }
 
 main() {
