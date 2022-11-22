328a3a247502d5a08a79cfe43240b4cbfdcb76f9
Gabriele Marconi <gabry.thestikinit@gmail.com>
2022-11-22 19:26:52 +0100

    Created GUI SpringBoot proj and Kotlin WS client

---
+++ sprint3/wsdemoNoSTOMP/wsdemoNoSTOMP/src/main/java/it/unibo/wsdemoNoSTOMP/TestClient.java
@@ -0,0 +1,26 @@
+package it.unibo.wsdemoNoSTOMP;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class TestClient {
+
+    public static void main(String[] args) throws URISyntaxException {
+        // 1) open websocket
+        WebsocketClientEndpoint clientEndPoint;
+        clientEndPoint = new WebsocketClientEndpoint(new URI("ws://localhost:8085"));
+
+        // 2) add listener
+        clientEndPoint.addMessageHandler(new IMessageHandler() {
+            public void handleMessage(String message) {
+                System.out.println(message);
+            }
+        });
+
+        // 3) send message to websocket
+        clientEndPoint.sendMessage("hello from Java client");
+
+
+
+    }
+}
---
+++ sprint3/wsdemoNoSTOMP/wsdemoNoSTOMP/src/main/resources/static/wsminimal.js
@@ -0,0 +1,37 @@
+var socket = connect();
+
+function connect(){
+    var host     = document.location.host;
+    var pathname =  document.location.pathname;
+    var addr     = "ws://" + host + pathname + "socket"  ;
+
+    // Assicura che sia aperta un unica connessione
+    if(socket!==undefined && socket.readyState!==WebSocket.CLOSED){
+        alert("WARNING: Connessione WebSocket già stabilita");
+    }
+    var socket = new WebSocket(addr); //CONNESSIONE
+
+    socket.onopen = function (event) {
+        addMessageToWindow("Connected");
+    };
+    socket.onmessage = function (event) { //RICEZIONE
+        addMessageToWindow(`Got Message: ${event.data}`);
+    };
+    return socket;
+}//connect
+
+const messageWindow   = document.getElementById("messageArea");
+const messageInput    = document.getElementById("inputmessage");
+const sendButton      = document.getElementById("send");
+
+sendButton.onclick = function (event) {
+    sendMessage(messageInput.value);
+    messageInput.value = "";
+}
+function sendMessage(message) {
+    socket.send(message);
+    addMessageToWindow("Sent Message: " + message);
+}
+function addMessageToWindow(message) {
+    messageWindow.innerHTML += `<div>${message}</div>`
+}
