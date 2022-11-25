a9da47a5db111dc3cdfd1a2732f9aac023428bd0
Noesh <56556129+Noesh@users.noreply.github.com>
2022-11-25 13:25:21 +0100

    gui upd

--- sprint3/gui/js/test.js
+++ sprint3/gui/js/test.js
@@ -4,6 +4,9 @@ var curPlastQt = 0;
 var maxGlassQt = 150;
 var maxPlastQt = 150;
 
+const ledState = {OFF:0, ON:1, BLINK:2}
+var ledCurState = ledState.OFF;
+
 function init(){
   // HERE LOAD DATA FROM SERVER
   var gText = document.getElementById("glassText");
@@ -54,5 +57,54 @@ function updatePlastic(newPlastQt) {
         text.innerHTML = height + "/" + maxPlastQt;
       }
     }
+  }
+}
+
+function ledOn() {
+  if (newPlastQt > curPlastQt) {
+    var bar = document.getElementById("plasticBar");
+    var text = document.getElementById("plasticText");
+
+    var height = curPlastQt;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (height >= newPlastQt) {
+        clearInterval(id);
+        curPlastQt = newPlastQt;
+        text.innerHTML = curPlastQt + "/" + maxPlastQt;
+      } else {
+        height++;
+        var perc = height / maxPlastQt * 100;
+        bar.style.height = perc + "%";
+        text.innerHTML = height + "/" + maxPlastQt;
+      }
+    }
+  }
+}
+
+function ledOff() {
+  if(ledCurState !== ledState.OFF){
+    var led = document.getElementById("led");
+
+    led.className = "led-off"
+    ledCurState = ledState.OFF
+  }
+}
+
+function ledOn() {
+  if(ledCurState !== ledState.ON){
+    var led = document.getElementById("led");
+
+    led.className = "led-on"
+    ledCurState = ledState.ON
+  }
+}
+
+function ledBlink() {
+  if(ledCurState !== ledState.BLINK){
+    var led = document.getElementById("led");
+
+    led.className = "led-blink"
+    ledCurState = ledState.BLINK
   }
 }
