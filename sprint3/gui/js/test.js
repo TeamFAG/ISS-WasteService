c1cfbb1ef0f996dafe6b6412685a0afa3e2da1dd
Noesh <56556129+Noesh@users.noreply.github.com>
2022-11-28 10:13:33 +0100

    gui finished

--- sprint3/gui/js/test.js
+++ sprint3/gui/js/test.js
@@ -4,16 +4,29 @@ var curPlastQt = 0;
 var maxGlassQt = 150;
 var maxPlastQt = 150;
 
+// LED
 const ledState = {OFF:0, ON:1, BLINK:2}
 var ledCurState = ledState.OFF;
 
-function init(){
+// ROBOT
+var robCurPos = "HOME"
+
+// LOG
+let curLog = ""
+
+function init() {
   // HERE LOAD DATA FROM SERVER
   var gText = document.getElementById("glassText");
   gText.innerHTML = curGlassQt + "/" + maxGlassQt;
 
   var pText = document.getElementById("plasticText");
   pText.innerHTML = curPlastQt + "/" + maxPlastQt;
+
+  var log = document.getElementById("log");
+  log.innerHTML = curLog;
+
+  var robot = document.getElementById("robotPos");
+  robot.innerHTML = robCurPos;
 }
 
 function updateGlass(newGlassQt) {
@@ -28,6 +41,8 @@ function updateGlass(newGlassQt) {
         clearInterval(id);
         curGlassQt = newGlassQt;
         text.innerHTML = curGlassQt + "/" + maxGlassQt;
+
+        updateLog("glassQuantity(" + curGlassQt + "/" + maxGlassQt + ")")
       } else {
         height++;
         var perc = height / maxGlassQt * 100;
@@ -50,6 +65,8 @@ function updatePlastic(newPlastQt) {
         clearInterval(id);
         curPlastQt = newPlastQt;
         text.innerHTML = curPlastQt + "/" + maxPlastQt;
+
+        updateLog("plasticQuantity(" + curPlastQt + "/" + maxPlastQt + ")")
       } else {
         height++;
         var perc = height / maxPlastQt * 100;
@@ -66,6 +83,8 @@ function ledOff() {
 
     led.className = "led-off"
     ledCurState = ledState.OFF
+
+    updateLog("led(OFF)")
   }
 }
 
@@ -75,6 +94,8 @@ function ledOn() {
 
     led.className = "led-on"
     ledCurState = ledState.ON
+
+    updateLog("led(ON)")
   }
 }
 
@@ -84,5 +105,31 @@ function ledBlink() {
 
     led.className = "led-blink"
     ledCurState = ledState.BLINK
+
+    updateLog("led(BLINK)")
   }
+}
+
+function updatePosition(newPos) {
+  if(robCurPos !== newPos){
+    var pos = document.getElementById("robotPos");
+    robCurPos = newPos.toUpperCase();
+
+    pos.innerHTML = robCurPos;
+
+    updateLog("robotPosition(" + robCurPos + ")")
+  }
+} 
+
+function updateLog(newEntry) {
+  var log = document.getElementById("log");
+
+  curLog += ("</br>" + newEntry);
+
+  log.innerHTML = curLog;
+}
+
+function updPosTest() {
+  var pos = document.getElementById("textArea");
+  updatePosition(pos.value)
 }
